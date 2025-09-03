54571d62dcc60a725915b5d74ba96f26dd403366
Aluno Dendezeiros <aluno.den@senaibahia.com.br>
2025-09-03 20:46:55 -0300

    atividade

--- basic/src/main/java/org/example/atv2/Main.java
+++ basic/src/main/java/org/example/atv2/Main.java
@@ -3,6 +3,16 @@ package org.example.atv2;
 
 public class Main {
     public static void main(String[] args) {
+        Soma soma = new Soma();
+        Subtracao subtracao = new Subtracao();
+        Mutiplicacao mutiplicacao = new Mutiplicacao();
+        Divisao divisao = new Divisao();
+
+        System.out.println(soma.calcular(1,1));
+        System.out.println(subtracao.calcular(2,2));
+        System.out.println(mutiplicacao.calcular(3,3));
+        System.out.println(divisao.calcular(4,4));
+
 
     }
 }
---
+++ basic/src/main/java/org/example/atv2/Mutiplicacao.java
@@ -0,0 +1,9 @@
+package org.example.atv2;
+
+public class Mutiplicacao implements OperacaoMatematica{
+    @Override
+    public double calcular(double a, double b) {
+        return a * b;
+    }
+}
+
